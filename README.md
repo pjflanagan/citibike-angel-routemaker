f88d154aaabd7ff6193807df0846c7df0ac46809
Peter Flanagan <pflanagan@freewheel.com>
2019-10-01 15:32:07 -0400

    map starter

---
+++ README.md
@@ -0,0 +1,6 @@
+# Citibike Angel 
+
+This program is designed to devise the best route to take rack up Citibike reward points
+
+- [Rewards](https://www.citibikenyc.com/bikeangels/rewards)
+- [Station Endpoint](https://layer.bicyclesharing.net/map/v1/nyc/stations)
---
+++ src/map.js
@@ -0,0 +1,75 @@
+/**
+ * @class Layers
+ */
+
+const Features = {
+  map: function ({ lng, lat, zoom }) {
+    return {
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [lng, lat],
+      zoom: zoom
+    };
+  },
+
+  geolocateControl: function () {
+    return {
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserLocation: true
+    };
+  },
+
+  point: function (lngLat, i) {
+    return {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          lngLat.lng,
+          lngLat.lat
+        ]
+      },
+      "properties": {
+        "id": i
+      }
+    }
+  },
+}
+
+class MapDirector {
+  constructor(lng, lat, zoom) {
+    const self = this;
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGpmbGFuYWdhbiIsImEiOiJjampldWRtMDQ0YWc1M3ByNXhwemNqa25kIn0.UGCAQuunCGs-9X9q5b1PZg';
+    this.map = new mapboxgl.Map(Features.map({ lng, lat, zoom }));
+
+    // gelocate
+    this.geolocateControl = new mapboxgl.GeolocateControl(Features.geolocateControl());
+    this.map.addControl(this.geolocateControl);
+
+    // this.map.on("load", function () { self.init(); });
+  }
+
+  getPosition() {
+    let mapBounds = this.map.getBounds();
+    let bounds = {
+      "min": mapBounds._sw,
+      "max": mapBounds._ne
+    };
+    let center = this.map.getCenter();
+    let zoom = this.map.getZoom();
+    return { bounds, center, zoom };
+  }
+
+  locate() {
+    this.geolocateControl.trigger();
+  }
+
+}
+
+window.onload = function() {
+  const map = new MapDirector(0,0,2);
+}
